bc78ac77cdeba72f3fac980c1d623bf10e88b899
janardhankandregula <janardhank134@gmail.com>
2024-06-27 00:58:57 -0400

    episode-

---
+++ App.js
@@ -0,0 +1,19 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "tag" }, "iam h1 tag"),
+    React.createElement("h2", { id: "adjacent" }, "iam adjacent h1 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "tag" }, "iam h1 tag"),
+    React.createElement("h2", { id: "adjacent" }, "iam adjacent h1 tag"),
+  ]),
+]);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Hello world from React!"
+// );
+// console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
