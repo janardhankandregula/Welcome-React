8f3df3116c3eb56affe97119694d3718f2dc16c7
janardhankandregula <janardhank134@gmail.com>
2024-06-29 06:08:16 -0400

    success

--- App.js
+++ App.js
@@ -20,22 +20,53 @@ const parent = <h1 className="test">welcome world</h1>;
 //   "Hello world from React!"
 // );
 // console.log(heading);
+// const TitleComponent = (props) => <h1>{props.a + props.b}</h1>;
 
-const HeadingComponent2 = () => (
-  <div>
-    <h1>Iam component without return statement</h1>
+// const HeadingComponent2 = () => (
+//   <div>
+//     <h1>Iam component without return statement</h1>
+//   </div>
+// );
+
+// const HeadingComponent = () => {
+//   return (
+//     <div>
+//       <h1>Iam Functional component</h1>
+//       <HeadingComponent2 />
+
+//       {TitleComponent(2, 3)}
+//     </div>
+//   );
+// };
+
+const HeaderComponet = React.createElement("div", { className: "title" }, [
+  React.createElement("h1", {}),
+  React.createElement("h2", {}),
+  React.createElement("h3", {}),
+]);
+
+const HeaderComponent2 = (
+  <div className="title">
+    <h1>HeaderComponent2</h1>
+    <h2>HeaderComponent2</h2>
+    <h3>Header componet2</h3>
   </div>
 );
 
-const HeadingComponent = () => {
+const HeaderFunctionalComponent = () => {
   return (
     <div>
-      <h1>Iam Functional component</h1>
-      <HeadingComponent2 />
+      <h1>Functional component</h1>
+      <h2>Functional component</h2>
+      <h3>Functional component</h3>
+      {/* {console.log(HeaderComponent2)} */}
+      <h2>{2 + 3}</h2>
+      {HeaderComponent2}
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
-root.render(<HeadingComponent />);
+// root.render(HeaderComponent2);
+// root.render(<HeaderComponent2 />);
+root.render(<HeaderFunctionalComponent />);
